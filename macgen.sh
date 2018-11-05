27c1e93063d7483332178a2c8d623550f1c6a33c
Podrepny <31345196+Podrepny@users.noreply.github.com>
2018-11-05 18:40:47 +0200

    fix some bug

--- macgen.sh
+++ macgen.sh
@@ -12,8 +12,29 @@ MIN_COUNT="1"
 MAX_COUNT="100"
 MAC_PREFIX_DEFAULT="52:54:00:"
 
-if [[ $MAC_PREFIX =~ ^([[:xdigit:]]{2}:){3}$ ]]; then echo "Use custom mac prefix $MAC_PREFIX"; else echo "Use default mac prefix ($MAC_PREFIX_DEFAULT)"; MAC_PREFIX=$MAC_PREFIX_DEFAULT; fi
-if ! [[ "$MAC_COUNT" =~ ^[0-9]+$ ]]; then echo "Sorry integers only"; exit 1; fi
-if [ "$MAC_COUNT" -le "$MIN_COUNT" -o "$MAC_COUNT" -ge "$MAX_COUNT" ]; then MAC_COUNT=1; echo "Range from $MIN_COUNT to $MAX_COUNT only"; fi
+# Check MAC prefix
+if [[ $MAC_PREFIX =~ ^([[:xdigit:]]{2}:){3}$ ]]; then
+    echo -e "Use custom MAC prefix $MAC_PREFIX"
+else
+#    echo -e "MAC prefix must be FF:FF:FF: \nUse default MAC prefix ($MAC_PREFIX_DEFAULT)"
+    MAC_PREFIX=$MAC_PREFIX_DEFAULT
+fi
 
-for ((n=0;n<$MAC_COUNT;n++)); do   MAC_GEN=$MAC_PREFIX`(date; cat /proc/interrupts) | md5sum | sed -r 's/^(.{6}).*$/\1/; s/([0-9a-f]{2})/\1:/g; s/:$//;'`; echo $MAC_GEN; done
+# Check MAC count is an integer
+if ! [[ "$MAC_COUNT" =~ ^[0-9]+$ ]]; then
+#    echo "MAC count must be integer"
+    MAC_COUNT=1
+fi
+
+# Check MAC count range
+if [ "$MAC_COUNT" -lt "$MIN_COUNT" -o "$MAC_COUNT" -gt "$MAX_COUNT" ]; then
+    MAC_COUNT=1
+    echo "Range from $MIN_COUNT to $MAX_COUNT only"
+fi
+
+echo -e "\e[97m"
+for ((n=0;n<$MAC_COUNT;n++)); do
+    MAC_GEN=$MAC_PREFIX`(date; cat /proc/interrupts) | md5sum | sed -r 's/^(.{6}).*$/\1/; s/([0-9a-f]{2})/\1:/g; s/:$//;'`
+    echo -e "\e[32m$MAC_GEN"
+done
+echo -e "\e[97m"
