224e734e362e89c168b6c3de58880ce488d02252
Alexey <42038886+opidrepnyi@users.noreply.github.com>
2018-11-05 14:09:57 +0200

    patch if prefix don't set

--- macgen.sh
+++ macgen.sh
@@ -12,7 +12,7 @@ MIN_COUNT="1"
 MAX_COUNT="100"
 MAC_PREFIX_DEFAULT="52:54:00:"
 
-if [[ $MAC_PREFIX =~ ^([[:xdigit:]]{2}:){3}$ ]]; then echo "Use custom mac prefix $MAC_PREFIX"; else echo "Use default mac prefix ($MAC_PREFIX_DEFAULT)"; fi
+if [[ $MAC_PREFIX =~ ^([[:xdigit:]]{2}:){3}$ ]]; then echo "Use custom mac prefix $MAC_PREFIX"; else echo "Use default mac prefix ($MAC_PREFIX_DEFAULT)"; MAC_PREFIX=$MAC_PREFIX_DEFAULT; fi
 if ! [[ "$MAC_COUNT" =~ ^[0-9]+$ ]]; then echo "Sorry integers only"; exit 1; fi
 if [ "$MAC_COUNT" -le "$MIN_COUNT" -o "$MAC_COUNT" -ge "$MAX_COUNT" ]; then MAC_COUNT=1; echo "Range from $MIN_COUNT to $MAX_COUNT only"; fi
 
